6dcea66148745d01bde984f67d5d9c0d6778720a
Kylene Naiomi Bondoc <krbondoc@student.apc.edu.ph>
2023-02-16 13:31:01 +0800

    Static Pages

---
+++ ci4/app/Views/pages/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Home: Kylene's CodeIgniter</h1>
+
+<?php
+echo "Welcome to my Furnace!";
+?>
+
+</body>
+</html>
---
+++ ci4/app/Views/templates/header.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+<head>
+    <title>CodeIgniter Tutorial: Bondoc, Kylene</title>
+</head>
+<body>
+
+    <h1><?= esc($title) ?></h1>
