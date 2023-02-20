6f140c4768c27e3cff70929d5aa83a12ea6c3bbf
Kylene Naiomi Bondoc <krbondoc@student.apc.edu.ph>
2023-02-20 12:24:33 +0800

    Code Igniter trial

--- ci4/app/Controllers/Pages.php
+++ ci4/app/Controllers/Pages.php
@@ -22,15 +22,3 @@ class Pages extends BaseController
             . view('templates/footer');
     }
 }
-class Pages extends BaseController
-{
-    public function index()
-    {
-        return view('welcome_message');
-    }
-
-    public function view($page = 'home')
-    {
-        // ...
-    }
-}
