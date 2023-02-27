e57121b98b1aa7f78eda4be28854c6536e3bdea0
Kylene Naiomi Bondoc <krbondoc@student.apc.edu.ph>
2023-02-27 23:19:33 +0800

    News Section completion

--- ci4/app/Controllers/News.php
+++ ci4/app/Controllers/News.php
@@ -3,21 +3,6 @@
 namespace App\Controllers;
 
 use App\Models\NewsModel;
-use CodeIgniter\Exceptions\PageNotFoundException;
-
-class News extends BaseController
-{
-    public function index()
-    {
-        $model = model(NewsModel::class);
-
-        $data['news'] = $model->getNews();
-
-        <?php
-
-namespace App\Controllers;
-
-use App\Models\NewsModel;
 
 class News extends BaseController
 {
